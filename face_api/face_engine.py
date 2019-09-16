c3ffc251a5b38f4be17bf8e7254dbe5cdb2fafa1
Achraf KHAZRI <A.khazri@openbee.com>
2019-09-16 09:33:28 +0100

    :sparkles: method for dataset creation for dlib recognition

--- face_api/face_engine.py
+++ face_api/face_engine.py
@@ -1,10 +1,19 @@
 import cv2 as cv
 import face_recognition
+import os
+import pandas as pd
+import numpy as np
+import pickle
 
 
 class faceEngine():
 
     def cascade_classifier_detector(self, img):
+        """
+        Face detection using Haar cascade classifier with OpenCv
+        :param img: input image
+        :return: faces bboxes
+        """
 
         # Load Haar Cascade Classifier
         face_cascade = cv.CascadeClassifier('data/haarcascade_frontalface_default.xml')
@@ -29,6 +38,11 @@ class faceEngine():
         cv.destroyAllWindows()
 
     def dlib_detector(self, img):
+        """
+        Face detection using Dlib
+        :param img: input image
+        :return: faces bboxes
+        """
 
         # Find all the faces in the image
         face_locations = face_recognition.face_locations(img)
@@ -50,28 +64,83 @@ class faceEngine():
         cv.waitKey(0)
         cv.destroyAllWindows()
 
-    def dlib_recognition(self):
+    def dlib_recognition(self, img):
+        """
+        Face recognition using Dlib
+        :param img: input image
+        :return: Names of the detected persons
+        """
+        output_name = "No_Body"
 
+        # Load dataset
+        dataset = pd.read_pickle("data/data.pkl")
+
+        encodings = dataset["encodings"].values
+        names = dataset["names"].values
         # Load image
         jobs = face_recognition.load_image_file("data/jobs1.jpg")
-        obama = face_recognition.load_image_file("data/obama1.jpg")
-        einstein = face_recognition.load_image_file("data/einstein1.jpg")
-        unknown_image = face_recognition.load_image_file("data/obama3.jpg")
 
         # Encode facial features
         jobs_encoding = face_recognition.face_encodings(jobs)[0]
-        obama_encoding = face_recognition.face_encodings(obama)[0]
-        einstein_encoding = face_recognition.face_encodings(einstein)[0]
-        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+        i = 0
 
-        # Compare features
-        results = face_recognition.compare_faces([einstein_encoding, obama_encoding, jobs_encoding], unknown_encoding)
+        for encode in encodings:
 
-        return results
+            result = face_recognition.compare_faces([encode], jobs_encoding)
+
+            if result[0] == True:
+                output_name = names[i]
+
+            i += 1
+
+        return output_name
+
+    def create_dataset(self, folder_path):
+
+        # Faces encoding features
+        features = []
+
+        # Persons names extracted from images names
+        names = []
+
+        # Filter all files in the directory
+        for filename in os.listdir(folder_path):
+
+            # Make sure that our file is text
+            if (filename.endswith('.jpeg')) or (filename.endswith('.jpg')) or (filename.endswith('.png')):
+
+                # Load image
+                image = face_recognition.load_image_file(folder_path + "/" + filename)
+
+                # Encode face features
+                feature = face_recognition.face_encodings(image)[0]
+
+                # Appen feature to total features list
+                features.append(feature)
+
+                # Add person name to dataset
+                names.append(os.path.splitext(filename)[0])
+
+                print("Encoded <<<>>>" + os.path.splitext(filename)[0] + "<<<>>>")
+
+        # Save dataset
+        data = pd.DataFrame({"encodings": features, "names": names})
+        pd.to_pickle(data, "data/data.pkl")
+
+
+
+    def add_face(self):
+
+        return 0
 # Load the jpg file into a numpy array
 image = cv.imread("data/test.jpg")
 
 fd = faceEngine()
 
-out = fd.dlib_recognition()
+# Folder that contains all the images
+folder_path = "data/dataset"
+
+#fd.create_dataset(folder_path)
+
+out = fd.dlib_recognition(image)
 print(out)
