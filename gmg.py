4ecb5c3c9f185854d58d7b03364e54c66bd8a7f4
Achraf KHAZRI <A.khazri@openbee.com>
2019-09-17 22:41:01 +0100

    :sparkles: Adding new face detectors

--- gmg.py
+++ gmg.py
@@ -8,37 +8,66 @@ import sys
 import argparse
 import os
 import glob
-from face_api.face_engine import faceEngine
+from face_api.face_engine import *
 
 # Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--t', '-task', help="Classifier mode : task or train", type= str)
 parser.add_argument('--p', '-path', help="File path", type= str)
+parser.add_argument('--m', '-method', help="Face detection method", type= str)
 
 # Get args
 args = parser.parse_args()
 
 # Object instance
-fe = faceEngine()
-
+fe = DlibHOGFaceDetector()
+haar = OpenCVHaarFaceDetector()
 # Face detection
 if args.t == "face_detection":
-    faces = fe.dlib_detector(args.p)
+
+    # Initialize method
+    if args.m == 'opencv_haar':
+        face_detector = OpenCVHaarFaceDetector()
+        faces = face_detector.cascade_classifier_detector(args.p)
+
+    elif args.m == 'dlib_hog':
+        face_detector = DlibHOGFaceDetector()
+        faces = face_detector.face_detector(args.p)
+
+    elif args.m == 'dlib_cnn':
+        face_detector = DlibCNNFaceDetector()
+        faces = face_detector.detect_face(args.p)
+
+    elif args.m == 'mtcnn':
+        face_detector = TensorflowMTCNNFaceDetector()
+        faces = face_detector.detect_face(args.p)
+
+    elif args.m == 'mobilenet_ssd':
+        face_detector = TensoflowMobilNetSSDFaceDector()
+        faces = face_detector.detect_face(args.p)
+
+    else:
+        print("Error detection method !")
+
     print("Number of detected people : " + str(len(faces)))
+
 
 # Face recognition
 elif args.t == "face_recognition":
-    person = fe.dlib_recognition(args.p)
+
+    face_recogniser = DlibHOGFaceDetector()
+    person = face_recogniser.dlib_recognition(args.p)
     print("Person : " + person)
 
 # Initialise dataset to images in face_api/data/dataset
 elif args.t == "face_init":
-    person = fe.create_dataset()
-    print("Done !")
+    face_recogniser = DlibHOGFaceDetector()
+    person = face_recogniser.create_dataset()
 
 # Add new person to dataset
 elif args.t == "add_face":
-    fe.add_face(args.p)
+    face_recogniser = DlibHOGFaceDetector()
+    face_recogniser.add_face(args.p)
 
 else:
     print("Error command !")
