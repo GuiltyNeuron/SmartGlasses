39577b4a4516c51d2dc5711d1324917489bfc1ea
Achraf KHAZRI <A.khazri@openbee.com>
2019-09-20 22:43:35 +0100

    :zap: improving processing performance by changing import location

--- gmg.py
+++ gmg.py
@@ -8,8 +8,8 @@ import sys
 import argparse
 import os
 import glob
-from face_api.face_engine import *
-from wiki_api.wiki_engine import WikiEngine
+
+
 # from ocr_api.ocr_engine import OcrEngine
 from speech_api.speech_engine import SpeechEngine
 
@@ -28,25 +28,30 @@ se = SpeechEngine()
 
 # Face detection
 if args.t == "face_detection":
-
+    from face_api.face_engine import OpenCVHaarFaceDetector
     # Initialize method
     if args.m == 'opencv_haar':
+        from face_api.face_engine import OpenCVHaarFaceDetector
         face_detector = OpenCVHaarFaceDetector()
         faces = face_detector.cascade_classifier_detector(args.i)
 
     elif args.m == 'dlib_hog':
+        from face_api.face_engine import DlibHOGFaceDetector
         face_detector = DlibHOGFaceDetector()
         faces = face_detector.face_detector(args.i)
 
     elif args.m == 'dlib_cnn':
+        from face_api.face_engine import DlibCNNFaceDetector
         face_detector = DlibCNNFaceDetector()
         faces = face_detector.detect_face(args.i)
 
     elif args.m == 'mtcnn':
+        from face_api.face_engine import TensorflowMTCNNFaceDetector
         face_detector = TensorflowMTCNNFaceDetector()
         faces = face_detector.detect_face(args.i)
 
     elif args.m == 'mobilenet_ssd':
+        from face_api.face_engine import TensoflowMobilNetSSDFaceDector
         face_detector = TensoflowMobilNetSSDFaceDector()
         faces = face_detector.detect_face(args.i)
 
@@ -59,23 +64,27 @@ if args.t == "face_detection":
 
 # Face recognition
 elif args.t == "face_recognition":
-
+    from face_api.face_engine import DlibHOGFaceDetector
     face_recogniser = DlibHOGFaceDetector()
     person = face_recogniser.dlib_recognition(args.i)
     print("Person : " + person)
+    se.text2speech("We detected " + person)
 
 # Initialise dataset to images in face_api/data/dataset
 elif args.t == "face_init":
+    from face_api.face_engine import DlibHOGFaceDetector
     face_recogniser = DlibHOGFaceDetector()
     person = face_recogniser.create_dataset()
 
 # Add new person to dataset
 elif args.t == "add_face":
+    from face_api.face_engine import DlibHOGFaceDetector
     face_recogniser = DlibHOGFaceDetector()
     face_recogniser.add_face(args.i)
 
 # Ask about somthing using wikipedia
 elif args.t == "wiki":
+    from wiki_api.wiki_engine import WikiEngine
     we = WikiEngine()
     infos = we.run(args.i, 3)
     print(infos)
