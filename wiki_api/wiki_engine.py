adb1560148a769ea5cfad80b54a0faccdb2b71ac
Achraf KHAZRI <A.khazri@openbee.com>
2019-09-21 14:27:47 +0100

    :zap: Adding multilanguage wiki getting infos

--- wiki_api/wiki_engine.py
+++ wiki_api/wiki_engine.py
@@ -4,16 +4,41 @@ import re
 
 class WikiEngine:
 
-    def __init__(self):
-        wikipedia.set_lang("en")
+    def __init__(self, lang):
+
+        self.lang = lang
+        if lang == "fr":
+            wikipedia.set_lang("fr")
+
+        if lang == "fr":
+            wikipedia.set_lang("fr")
 
     def run(self, object, sentences_number):
 
-        output = wikipedia.summary(object, sentences=sentences_number)
+        if self.lang == "en":
+            try:
+                output = wikipedia.summary(object, sentences=sentences_number)
 
-        # remove hexa chars from encoding problem
-        infos = re.sub(r'[^\x00-\x7f]',r'', output)
+                # remove hexa chars from encoding problem
+                # infos = re.sub(r'[^\x00-\x7f]',r'', output)
 
-        # removing listen key from text
-        infos = infos.replace("(listen)", " ")
+                # removing listen key from text
+                # infos = infos.replace("(listen)", " ")
+                infos = output
+            except:
+                infos = "Please reask your question in another form !"
+
+        if self.lang == "fr":
+            try:
+                output = wikipedia.summary(object, sentences=sentences_number)
+
+                # remove hexa chars from encoding problem
+                # infos = re.sub(r'[^\x00-\x7f]', r'', output)
+
+                # removing listen key from text
+                # infos = infos.replace("(écouter)", " ")
+                infos = output
+            except:
+                infos = "s'il vous plait reformulez votre question !"
+
         return infos
