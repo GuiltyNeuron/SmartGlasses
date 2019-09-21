fd6d73e8b61da6aec492c47f1c6abf93f9b0d10f
Achraf KHAZRI <A.khazri@openbee.com>
2019-09-22 00:26:02 +0100

    :boom: Adding news api

---
+++ news_api/news_engine.py
@@ -0,0 +1,39 @@
+import bs4 as bs
+import urllib.request
+import re
+
+
+class NewsEngine:
+
+    def __init__(self):
+
+        self.source = urllib.request.urlopen('https://edition.cnn.com/articles/').read()
+        self.base_url = "https://edition.cnn.com"
+        self.article_titles = []
+        self.article_links = []
+
+    def get_latest_articles(self):
+
+        soup = bs.BeautifulSoup(self.source, "lxml")
+
+        # Fetch through all div
+        for div in soup.find_all('div'):
+
+            # Get div class
+            cls = div.get('class')
+
+            # Check if class is a list, somtimes it have None type
+            if isinstance(cls, list):
+
+                # Get the first item of class list
+                if cls[0] == "cd__content":
+
+                    # Get article title
+                    a_child = div.findChildren("a", recursive=True)[0]
+                    self.article_links.append(self.base_url + str(a_child.get('href')))
+
+                    # Get article link
+                    span_child = div.findChildren("span", recursive=True)[0]
+                    self.article_titles.append(span_child.get_text())
+
+        return self.article_titles
