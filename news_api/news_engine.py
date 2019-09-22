4e624d23d45439c663f81ef7d462199325878e07
Achraf KHAZRI <A.khazri@openbee.com>
2019-09-22 02:26:43 +0100

    :sparkles: Getting a specified article by news engine

--- news_api/news_engine.py
+++ news_api/news_engine.py
@@ -36,4 +36,24 @@ class NewsEngine:
                     span_child = div.findChildren("span", recursive=True)[0]
                     self.article_titles.append(span_child.get_text())
 
-        return self.article_titles
+        return self.article_titles, self.article_links
+
+    def get_article(self, article_number):
+
+        articles, links = self.get_latest_articles()
+        source = urllib.request.urlopen(links[article_number - 1]).read()
+        soup = bs.BeautifulSoup(source, "lxml")
+
+        article_text = ""
+
+        # Fetch through all div
+
+        for div in soup.find_all("div", {"class": "el__leafmedia el__leafmedia--sourced-paragraph"}):
+
+            article_text = article_text + str(div.get_text()) + " "
+
+        for div in soup.find_all("div", {"class": "zn-body__paragraph"}):
+
+            article_text = article_text + str(div.get_text()) + " "
+
+        return article_text
